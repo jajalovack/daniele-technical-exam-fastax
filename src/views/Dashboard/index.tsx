e812c1a8eff9149ad672322d0b993b03e1d72a38
jajalovack <dvtejuco@gmail.com>
2023-12-26 23:42:52 +0800

    admins can delete users except self

--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -1,23 +1,38 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import Button from '../../components/Button'
+import Modal from '../../components/Modal'
 
 const Dashboard = () => {
+  const [modalDelete,setModalDelete]=useState(false)
+  const [modalCant,setModalCant]=useState(false)
+  const [userId,setUserId]=useState(0)
   const users = JSON.parse(String(localStorage.getItem('users')))
   const currentUser = JSON.parse(String(localStorage.getItem('currentUser')))
-
+  
   const deleteUser = (id: number) => {
     const findSelf=users.find((x: typeof users)=>x.id==id).id
     if (findSelf==currentUser.id)
     {
-      alert("You can't delete your own account while logged in.")
+      setModalCant(true)
       return
     }
+    else
+    {
+      setModalDelete(true)
+      return
+    }
+  }
+
+  const confirmDelete = (id: number) => {
     const newUsers=users.filter((x: typeof users)=>x.id!=id)
-    console.log(newUsers)
+    localStorage.setItem('users',JSON.stringify(newUsers))
   }
 
   return (
     <>
+      <Modal type='delete' open={modalDelete} onClose={()=>{setModalDelete(false)}} onDelete={()=>{confirmDelete(userId)}}/>
+      <Modal type='cant' open={modalCant} onClose={()=>{setModalCant(false)}} onDelete={()=>{}}/>
       <div className="flex flex-wrap justify-center">
         <div className="flex w-9/12 mt-2 mb-2">
           {
@@ -38,9 +53,9 @@ const Dashboard = () => {
             <div className="removeUser w-1/12"></div>
         </div>
         {
-          users.map((user: typeof users) => {
+          users.map((user: typeof users, index: number) => {
             return (
-              <div className='flex justify-center items-center w-9/12 mb-2'>
+              <div className='flex justify-center items-center w-9/12 mb-2' key={index}>
                 <div className="userId w-1/12 text-center">{user.id}</div>
                 <div className="branchId w-1/12 text-center">{user.branch_id}</div>
                 <div className="fullname w-2/12 text-center">{user.first_name} {user.last_name}</div>
@@ -48,7 +63,7 @@ const Dashboard = () => {
                 <div className="username w-1/12 text-center">{user.username}</div>
                 <div className="pword w-4/12 text-center overflow-x-auto">{user.password}</div>
                 <div className="accountType w-1/12 text-center">{user.account_type==0?'Viewer':'Admin'}</div>
-                <div className="removeUser w-1/12 text-center">{currentUser.account_type==1?<Button style='danger' onClick={() => {deleteUser(user.id)}}>Delete</Button>:<></>}</div>
+                <div className="removeUser w-1/12 text-center">{currentUser.account_type==1?<Button style='danger' onClick={() => {deleteUser(user.id); setUserId(user.id)}}>Delete</Button>:<></>}</div>
               </div>
             )
           })
